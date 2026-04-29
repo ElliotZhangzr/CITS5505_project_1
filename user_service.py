b699daf3d2d558d4583e0bf2c4f41092301d0d73
kushagra Patel <23685578@student.uwa.edu.au>
2026-04-29 20:50:16 +0800

    Add pagination to users page with improved structure

--- user_service.py
+++ user_service.py
@@ -1,15 +1,19 @@
 from models import User
 
+def get_users_paginated(page=1, per_page=5):
+    users = User.query.order_by(User.created_at.asc()).paginate(page=page, per_page=per_page)
 
-def get_all_users():
-    users = User.query.order_by(User.created_at.desc()).all()
-
-    return [
-        {
-            "id": user.id,
-            "username": user.username,
-            "email": user.email,
-            "joinTime": user.created_at.strftime("%d %b %Y, %I:%M %p"),
-        }
-        for user in users
-    ]
+    return {
+        "users": [
+            {
+                "id": user.id,
+                "username": user.username,
+                "email": user.email,
+                "joinTime": user.created_at.strftime("%d %b %Y, %I:%M %p"),
+            }
+            for user in users.items
+        ],
+        "has_next": users.has_next,
+        "has_prev": users.has_prev,
+        "page": page
+    }
