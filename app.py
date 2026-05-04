f0bf0ddf45eb1f9988af0037b058bf1eba521cc4
elliotzhang <18201349151@163.com>
2026-05-05 00:14:12 +0800

    fix bug

--- app.py
+++ app.py
@@ -242,5 +242,5 @@ def logout():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    # app.run(host="0.0.0.0", port=5000, debug=False)
+    # app.run(debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=False)
