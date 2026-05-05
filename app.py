b889a7a39fcacede6dd55c98e6f04569b44d9eba
elliotzhang <18201349151@163.com>
2026-05-05 23:07:56 +0800

    refactor: reorganize static assets into css/ and js/ and fix template paths

--- app.py
+++ app.py
@@ -244,5 +244,5 @@ def logout():
 
 
 if __name__ == "__main__":
-    # app.run(debug=True)
-    app.run(host="127.0.0.1", port=5000, debug=False)
+    app.run(debug=True)
+    # app.run(host="127.0.0.1", port=5000, debug=False)
